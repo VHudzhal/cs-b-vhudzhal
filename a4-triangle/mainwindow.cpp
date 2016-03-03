ecc9c78972fbd81ce9a4492af1360b83d97d5e89
VHudzhal <vladymyrlem@mail.ru>
2016-03-03 20:49:17 +0200

    Triangle

---
+++ a4-triangle/mainwindow.cpp
@@ -0,0 +1,116 @@
+#include <ui_mainwindow.h>
+#include "mainwindow.h"
+#include <string>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent),
+      ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+ui->outPut->setWindowTitle("Triangle Serpinsky");
+    H = QPixmap(680,510);
+    H.fill(Qt::transparent);
+    pen = QPen(getRandColor(),10);
+    pen.setWidth(2);
+    paint.begin(&H);
+    paint.setPen(pen);
+    paint.eraseRect(0,0,680,510);
+
+}
+
+MainWindow::~MainWindow()
+{
+    paint.end();
+}
+
+//double sideLength;
+//int order;
+//void drawTriangle(int x1, int y1, int x2, int y2, int x3, int y3);
+//void triangleSerpinsky(int x1, int y1, int x2, int y2, int x3, int y3, int order, double sideLength);
+/*
+ *        A fractal is a geometric figure with the property of self-similarity,that is
+ * composed of several parts, each of which is similar to the entire shape entirely. In mathematics,
+ * under a set of points is understood Fractals in Euclidean space with fractional
+ * metric dimension or metric dimension is different from.
+ *        The most famous example of a fractal geometry area is the Sierpinski triangle,
+ * by partitioning the triangle, not necessarily equilateral-medium lines at four such trianguli,
+ * except Central and recursive splitting angle triangles to obtain the area.
+*/
+
+QColor MainWindow::getRandColor()
+{
+    int r = rand()%255;
+    int g = rand()%255;
+    int b = rand()%255;
+    return QColor(r,g,b);
+}
+void MainWindow::drawTriangle(int x1, int y1, int x2, int y2, int x3, int y3)
+{
+
+    paint.drawLine(x1,y1,x2,y2);
+    paint.drawLine(x2,y2,x3,y3);
+    paint.drawLine(x3,y3,x1,y1);
+
+    ui->outPut->setPixmap(H);
+}
+
+void MainWindow::triangleSerpinsky(
+        int x1, int y1, int x2, int y2, int x3, int y3,
+        int order, // depth (how many more times do we recurse)//
+        double sideLength // contraction mapping constant
+        )
+{
+    /*
+                    B x2,y2
+                    / \
+                   / _ \
+                  / \  / \
+     x11,y11 /_ \/_ \ x22,y22
+                /\       / \
+               /_\     /__\
+              /\  /\  /\   /\
+    x1,y1 /_\/_ \/_\ /_\x3,y3
+ A                 C
+                    x33,
+                    y33
+*/
+    int x11,y11,x22,y22,x33,y33;
+
+    if(order >= 0)
+    {
+        x11 = (x1 + x2) / 2;
+        y11 = (y1 + y2) / 2;
+        x22 = (x2 + x3) / 2;
+        y22 = (y2 + y3) / 2;
+        x33 = (x3 + x1) / 2;
+        y33 = (y3 + y1) / 2;
+        drawTriangle(x11, y11, x22, y22, x33, y33);
+        triangleSerpinsky(x1, y1, x11, y11, x33, y33, order-1, sideLength);
+        triangleSerpinsky(x2, y2, x11, y11, x22, y22, order-1, sideLength);
+        triangleSerpinsky(x3, y3, x22, y22, x33, y33, order-1, sideLength);
+    }
+
+}
+
+//button for build triangle
+
+void MainWindow::on_drawButton_clicked()
+{
+    int order;
+    double sideLength;
+
+    QString str = ui->lineEdit->text();
+    QString str1 = ui->lineEdit_2->text();
+
+
+        sideLength = str.toDouble();
+
+       order = str1.toInt();
+
+    drawTriangle(50,450,350,50,650,450);
+    triangleSerpinsky(50,450,350,50,650,450, order - 1, sideLength);
+
+}
---
+++ a4-triangle/mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include <QMainWindow>
+#include <QLineEdit>
+#include <QPainter>
+#include <QLabel>
+#include <QPushButton>
+namespace Ui {
+class MainWindow;
+}
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+void drawTriangle(int x1, int y1, int x2, int y2, int x3, int y3);
+void triangleSerpinsky(int x1, int y1, int x2, int y2, int x3, int y3, int order, double sideLength);
+private slots:
+void on_drawButton_clicked();
+
+
+private:
+    Ui::MainWindow *ui;
+
+    QPixmap H;
+    QPainter paint;
+    QPen pen;
+QColor getRandColor();
+
+};
+#endif // MAINWINDOW_H
