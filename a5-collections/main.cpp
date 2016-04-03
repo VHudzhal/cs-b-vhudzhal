d92f26ebd06c531a84fbe87f19a51003bbb68bf2
VHudzhal <vladymyrlem@mail.ru>
2016-04-03 21:21:53 +0300

    Update main.cpp

--- a5-collections/main.cpp
+++ a5-collections/main.cpp
@@ -3,13 +3,14 @@
 #include <mystack.h>
 #include <myqueue.h>
 #include <mylist.h>
-//#include <mymap.h>
+#include <mymap.h>
+#include <algorithm>
 int main()
 {
     cout<<"----------------------------------------------------------"<<endl;
     cout<<"*************************MyVector*************************"<<endl;
-    MyVector<int> myVector_one;
-    for(int i = 0; i<50; i++)
+    MyVector<double> myVector_one;
+    for(double i = 11.25; i>0 && i<55; i++)
         myVector_one.push_back(i);
 
     myVector_one.pop_back();
@@ -19,7 +20,6 @@ int main()
 
     cout <<"Capacity myVector_one: "<<myVector_one.capacity()<<endl;
     cout<<"Size myVector_one: "<<myVector_one.size()<<endl;
-
     cout<<endl;
 
     for(int i=0;i<myVector_one.size();i++)
@@ -32,20 +32,22 @@ int main()
     cout << "vector_two = vector_one"<<endl;
 
 
-    MyVector<int> myVector_two=myVector_one;
+    MyVector<double> myVector_two=myVector_one;
     cout <<"Capacity myVector_two: "<<myVector_two.capacity()<<endl;
     cout<<"Size myVector_two: "<<myVector_two.size()<<endl;
-    for(int i=0;i<myVector_two.size();i++)
+    for(double i=0;i<myVector_two.size();i++)
         cout << " " << myVector_two[i];
 
     cout<<endl;
     cout<<endl;
-
-    MyVector<int> myVector_three(50,16);
-    cout<<"myVector_three(30,30)"<<endl;
-    for(int i=0;i<myVector_three.size();i++)
-        cout << " " << myVector_three[i];
-
+    int threesize;
+    cout << "Please enter size of third vector:\n";
+    cin >> threesize;
+    MyVector<int> myVector_three(threesize,16);
+    cout<<"myVector_three"<<endl;
+    for(int i=0;i < (myVector_three.size())/2;i++)
+        cout << " " << myVector_three[i] ;
+    // cout <<"\nsize:"<<" "<<myVector_three.size();
     cout<<endl;
     cout<<endl;
 
@@ -80,7 +82,6 @@ int main()
 
     firstTry.show();
 
-
     cout<<endl;
     cout<<"MyList<int> firstTry"<<endl;
     cout<<"List size : ";
@@ -88,32 +89,19 @@ int main()
     firstTry.show();
     cout<<endl;
 
-    firstTry.set(0,45);
-
-    firstTry.show();
-    MyList<string> secondTry;
-
-    cout<<"MyList<int> secondTry \n"<<endl;
-    cout << "-- push_front push_back test --" << endl;
-        secondTry.push_back("one");
-        secondTry.push_back("two");
-        secondTry.push_back("three");
-        secondTry.push_back("four");
-        secondTry.push_back("five");
-        secondTry.push_back("six");
-        secondTry.push_front("zero");
-        secondTry.push_front("neg-one");
-        secondTry.push_back("six");
-        
-
-cout << endl;
+    cout<<"MyList<int> secondTry"<<endl;
+    MyList<int> secondTry;
+    secondTry.add_back(30);
+    secondTry.add_back(70);
+    secondTry.add_back(60);
+    secondTry.show();
+    cout<<endl;
 
     cout<<"----------------------------------------------------------"<<endl;
     cout<<endl;
     cout<<endl;
     cout<<"----------------------------------------------------------"<<endl;
     cout<<"*************************MyStack*************************"<<endl;
-    //stack::push/pop
     MyStack<int> first_stack;
     int sum(10);
     for (int i=0; i<7; ++i)
@@ -121,33 +109,30 @@ cout << endl;
     cout << "Popping out elements...";
     while (!first_stack.isEmpty())
     {
-sum += first_stack.getTop();
+        sum += first_stack.getTop();
         first_stack.pop();
     }
     cout << "total:"<< sum << '\n';
     //stack::push/top
     MyStack<int> second_stack;
+    cout <<"second";
     second_stack.push(10);
     second_stack.push(20);
     second_stack.push(30);
     second_stack.push(40);
+    second_stack.push(52);
+    second_stack.push(46);
 
     cout<<"Stack size: "<<second_stack.size()<<endl;
     second_stack.print();
     cout<<endl;
     cout << "MyStack.pop() is now: \n";
     second_stack.pop();
-     cout<<"Stack pop()"<<endl;
+    cout<<"Stack pop()"<<endl;
     second_stack.pop();
     second_stack.pop();
     cout<<"Stack size: "<<second_stack.size()<<endl;
     second_stack.print();
-    // stack::empty
-    MyStack<string> third_stack;
-    third_stack.push("hello");
-    third_stack.pop();
-    third_stack.pop();
-    third_stack.print();
 
     cout<<"----------------------------------------------------------"<<endl;
     cout<<endl;
@@ -155,49 +140,51 @@ sum += first_stack.getTop();
     cout<<"----------------------------------------------------------"<<endl;
     cout<<"*************************MyQueue************************"<<endl;
     MyQueue<int> queue;
-        queue.push_back(7);
-        queue.push_back(8);
-        queue.push_back(9);
-        queue.push_back(10);
-        queue.push_back(11);
-        cout<<"Queue size: "<<queue.size()<<endl;
-        queue.show();
-        cout<<"Queue.pop()"<<endl;
-        queue.pop();
-        MyQueue<double> queue_1;
-        queue_1.push_back(5);
-        queue_1.push_back(10);
-        queue_1.push_back(15);
-        queue_1.push_back(20);
-        queue_1.push_back(25);
-        queue_1.push_back(30);
-        queue_1.push_back(35);
-        queue_1.show();
+    MyQueue<double> queue_1;
 
-cout<<"----------------------------------------------------------"<<endl;
-cout<<endl;
-cout<<endl;
-cout<<"----------------------------------------------------------"<<endl;}
-//cout<<"*************************MyMap************************"<<endl;
-//MyMap <string,int> map;
-//map.put("Smith", 30);
-//map.put("Anderson", 31);
-//map.put("Lewis", 29);
-//map.put("Cook", 29);
-//map.put("Smith", 65);
+    queue.push_back(7);
+    queue.push_back(8);
+    queue.push_back(9);
+    queue.push_back(10);
+    queue.push_back(11);
+    queue.show();
+    cout<<"Queue size: "<<queue.size()<<endl;
+    cout<<"Queue.pop()"<<endl;
+    queue.pop();
+    queue_1.push_back(5);
+    queue_1.push_back(10);
+    queue_1.push_back(15);
+    queue_1.push_back(20);
+    queue_1.push_back(25);
+    queue_1.push_back(30);
+    queue_1.push_back(35);
+    queue_1.show();
 
-//cout << "Entries in map: " << map.toString(30) << endl;
-//cout << "The age for " << "Lewis is " <<
-//map.get("Lewis") << endl;
-//cout << "Is Smith in the map? " <<
-//(map.containsKey("Smith") ? "true" : "false") << endl;
-//cout << "Is age 33 in the map? " <<
-//(map.containsValue(33) ? "true" : "false") << endl;
 
-//map.remove("Smith");
-//cout << "Entries in map: " << map.toString(25) << endl;
+    cout<<"----------------------------------------------------------"<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<"----------------------------------------------------------"<<endl;
+    cout<<"*************************MyMap************************"<<endl;
+    //MyMap <string,int> map;
+    //map.put("Smith", 30);
+    //map.put("Anderson", 31);
+    //map.put("Lewis", 29);
+    //map.put("Cook", 29);
+    //map.put("Smith", 65);
 
-//map.clear();
-//cout << "Entries in map: " << map.toString() << endl;
-//   return 0;
-//}
+    //cout << "Entries in map: " << map.toString(30) << endl;
+    //cout << "The age for " << "Lewis is " <<
+    //map.get("Lewis") << endl;
+    //cout << "Is Smith in the map? " <<
+    //(map.containsKey("Smith") ? "true" : "false") << endl;
+    //cout << "Is age 33 in the map? " <<
+    //(map.containsValue(33) ? "true" : "false") << endl;
+
+    //map.remove("Smith");
+    //cout << "Entries in map: " << map.toString(25) << endl;
+
+    //map.clear();
+    //cout << "Entries in map: " << map.toString() << endl;
+    //   return 0;
+    }
