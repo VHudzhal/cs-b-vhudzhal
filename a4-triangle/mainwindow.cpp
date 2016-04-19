0d4a4672df1a0fe9b0ec240d07cab513e27deca5
VHudzhal <vladymyrlem@mail.ru>
2016-04-19 11:22:54 +0300

    Update mainwindow.cpp

--- a4-triangle/mainwindow.cpp
+++ a4-triangle/mainwindow.cpp
@@ -10,7 +10,7 @@ MainWindow::MainWindow(QWidget *parent)
       ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->outPut->setWindowTitle("Triangle Serpinsky");
+ui->outPut->setWindowTitle("Triangle Serpinsky");
     H = QPixmap(680,510);
     H.fill(Qt::transparent);
     pen = QPen(getRandColor(),10);
@@ -18,17 +18,13 @@ MainWindow::MainWindow(QWidget *parent)
     paint.begin(&H);
     paint.setPen(pen);
     paint.eraseRect(0,0,680,510);
+
 }
 
 MainWindow::~MainWindow()
 {
+    paint.end();
 }
-
-double sideLength;
-int order;
-void drawTriangle(int x1, int y1, int x2, int y2, int x3, int y3);
-void triangleSerpinsky(int x1, int y1, int x2, int y2, int x3, int y3, int order, double sideLength);
-paint.end();
 
 /*
  *        A fractal is a geometric figure with the property of self-similarity,that is
@@ -40,17 +36,6 @@ paint.end();
  * except Central and recursive splitting angle triangles to obtain the area.
 */
 
-void MainWindow::drawTriangle(int x1, int y1, int x2, int y2, int x3, int y3)
-{
-    QPixmap pix(680,510);
-    pix.fill(Qt::transparent);
-    QPainter paint;
-    QPen pen(Qt::green);
-    pen.setWidth(2);
-    paint.begin(&pix);
-    paint.setPen(pen);
-    paint.eraseRect(0,0,680,510);
-
 QColor MainWindow::getRandColor()
 {
     int r = rand()%255;
@@ -58,38 +43,22 @@ QColor MainWindow::getRandColor()
     int b = rand()%255;
     return QColor(r,g,b);
 }
+
+//the process of creating the shape of a triangle
 void MainWindow::drawTriangle(int x1, int y1, int x2, int y2, int x3, int y3)
 {
 
     paint.drawLine(x1,y1,x2,y2);
     paint.drawLine(x2,y2,x3,y3);
     paint.drawLine(x3,y3,x1,y1);
-    paint.end();
+
     ui->outPut->setPixmap(H);
 }
 
+//process drawing of Sierpinski triangle
 void MainWindow::triangleSerpinsky(
         int x1, int y1, int x2, int y2, int x3, int y3,
         int order, // depth (how many more times do we recurse)//
-        double sideLength // contraction mapping constant)
-        )
-{
-    /*
-           B
-          /\
-         /__\
-        /\  /\
-       /__\/__\
-      /\      /\
-     /__\    /__\
-    /\  /\  /\  /\
-   /__\/__\/__\/__\
- A                  C
-*/
-
-    int x11,y11,x22,y22,x33,y33;
-    if(order == 0)
-=======
         double sideLength // contraction mapping constant
         )
 {
@@ -102,7 +71,7 @@ void MainWindow::triangleSerpinsky(
                 /\      /\
                /__\    /__\
               /\  /\  /\  /\
-       x1,y1 /__\/__\/__\/__\x3,y3
+       x1,y1 /__\/__\/__\/__\/x3,y3
  A                 C
                     x33,
                     y33
@@ -130,26 +99,14 @@ void MainWindow::on_drawButton_clicked()
 {
     int order;
     double sideLength;
-    
-    lineEdit = new QLineEdit;
-    lineEdit_2 = new QLineEdit;
-    
-    QString str = ui->lineEdit->text();
-    QString str1 = ui->lineEdit_2->text();
-    
-    sideLength = str.toInt();
-    order = str1.toInt();
-    drawTriangle(50,450,350,50,650,450);
-    triangleSerpinsky(50,450,350,50,650,450, order - 1, sideLength);
-    update();
-
 
     QString str = ui->lineEdit->text();
     QString str1 = ui->lineEdit_2->text();
-    
+
     sideLength = str.toDouble();
     order = str1.toInt();
 
     drawTriangle(50,450,350,50,650,450);
     triangleSerpinsky(50,450,350,50,650,450, order - 1, sideLength);
+
 }
